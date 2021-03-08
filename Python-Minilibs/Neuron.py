81f5ec961bca08a477f02005fda09587b2bce5e9
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2021-03-07 21:08:02 -0500

    Update Neuron.py

--- Python-Minilibs/Neuron.py
+++ Python-Minilibs/Neuron.py
@@ -1,20 +1,19 @@
-### Microlibrary for Python, defines functions to create a neuron - Brendan Scott 2021 ###
 class Neuron:
-	def InitValues(WeightCount):
-		Weights = [0] * WeightCount
-		for WeightEnum in range(len(Weights)):
-			Weights[WeightEnum - 1] = (random.random() * 2) - 1
-		return Weights
-	def Guess(Weights, Input):
-		sum = 0
-		for WeightEnum2 in range(len(Weights)):
-			sum += Input[WeightEnum2 - 1] * Weights[WeightEnum2 - 1]
-		return Neuron.activate(sum)
-	def Train(Weights, Inputs, Target):
-		Error = Target - Neuron.Guess(Inputs)
-		for WeightEnum3 in range(len(Weights)):
-			Weights[WeightEnum3 - 1] += Error * Inputs[WeightEnum3 - 1] * LearnRate
-		return Weights
+	def InitValues(a):
+		b = [0] * a
+		for C in range(len(b)):
+			b[C - 1] = (random.random() * 2) - 1
+		return b
+	def Guess(b, Input):
+		J = 0
+		for D in range(len(b)):
+			J += Input[D - 1] * b[D - 1]
+		return Neuron.activate(J)
+	def Train(b, G, I):
+		F = I - Neuron.Guess(G)
+		for E in range(len(b)):
+			b[E - 1] += F * G[E - 1]
+		return b
 	def activate(num):
 		if (num >= 0) : return 1 
 		else : return -1
