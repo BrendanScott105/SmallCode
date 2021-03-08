e623b720b16eef3740451bbf1d12039b05cf900c
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2021-03-07 21:08:35 -0500

    Update Neuron.py

--- Python-Minilibs/Neuron.py
+++ Python-Minilibs/Neuron.py
@@ -4,16 +4,16 @@ class Neuron:
 		for C in range(len(b)):
 			b[C - 1] = (random.random() * 2) - 1
 		return b
-	def Guess(b, Input):
+	def Guess(b, K):
 		J = 0
 		for D in range(len(b)):
-			J += Input[D - 1] * b[D - 1]
+			J += K[D - 1] * b[D - 1]
 		return Neuron.activate(J)
 	def Train(b, G, I):
 		F = I - Neuron.Guess(G)
 		for E in range(len(b)):
 			b[E - 1] += F * G[E - 1]
 		return b
-	def activate(num):
-		if (num >= 0) : return 1 
+	def activate(L):
+		if (L >= 0) : return 1 
 		else : return -1
