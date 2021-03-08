282e49430bdd4f453ee1493e75a03fc58e6f8d88
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2021-03-07 22:57:00 -0500

    Redefine for ease of use

--- Python-Minilibs/Neuron.py
+++ Python-Minilibs/Neuron.py
@@ -1,18 +1,18 @@
 class Neuron:
-	def Init(a):
-		b = [0] * a
+	def Init(Weights):
+		b = [0] * Weights
 		for C in range(len(b)):
 			b[C - 1] = (random.random() * 2) - 1
 		return b
-	def Guess(b, K):
+	def Guess(ArrIn, Guess):
 		J = 0
-		for D in range(len(b)):
-			J += K[D - 1] * b[D - 1]
+		for D in range(len(ArrIn)):
+			J += Guess[D - 1] * ArrIn[D - 1]
 		return Neuron.M(J)
-	def Train(b, G, I):
-		for E in range(len(b)):
-			b[E - 1] += I - Neuron.Guess(G) * G[E - 1]
-		return b
+	def Train(ArrIn, Data, Val):
+		for E in range(len(ArrIn)):
+			ArrIn[E - 1] += Val - Neuron.Guess(ArrIn, Data) * Data[E - 1]
+		return ArrIn
 	def M(L):
 		if (L >= 0) : return 1 
 		else : return -1
